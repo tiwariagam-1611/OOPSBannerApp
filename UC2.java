c97a368541974dd9598f3107fa1bfd5dc391ad11
tiwariagam-1611 <tiwariagam16112003@gmail.com>
2026-02-17 16:28:23 +0530

    added uc2

---
+++ UC2.java
@@ -0,0 +1,29 @@
+/**
+* OOPSBannerApp UC2
+*
+* This class demonstrates a simple Java application that displays the Object
+* Oriented Programming System OOPS acronym to the console.
+*
+* @author Developer
+* @version 2.0
+*/
+
+
+
+public class UC2
+{
+	public static void main(String[] args)
+	{
+
+		System.out.println("   ***   " + " " + "   ***    " + "******    " + "   *****  ");
+		System.out.println(" **   ** " + " " + " **   **  " + "**    **  " + " **       ");
+		System.out.println("**     **" + " " + "**     ** " + "**     ** " + "**        ");
+		System.out.println("**     **" + " " + "**     ** " + "**    **  " + " **       ");
+		System.out.println("**     **" + " " + "**     ** " + "******    " + "   ***    ");
+		System.out.println("**     **" + " " + "**     ** " + "**        " + "      **  ");
+		System.out.println("**     **" + " " + "**     ** " + "**        " + "       ** ");
+		System.out.println(" **   ** " + " " + " **   **  " + "**        " + "      **  ");
+		System.out.println("   ***   " + " " + "   ***    " + "**        " + " *****    ");
+
+	}
+}
